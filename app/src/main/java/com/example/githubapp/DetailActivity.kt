d51f261292a423ce5c612a4c0ceb5506a56f0f4e
rasyidin <raflirasyidin20@gmail.com>
2021-08-14 15:15:07 +0700

    Buat Detail Activity

---
+++ app/src/main/java/com/example/githubapp/DetailActivity.kt
@@ -0,0 +1,18 @@
+package com.example.githubapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubapp.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
